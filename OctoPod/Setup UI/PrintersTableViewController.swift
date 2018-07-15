f11c255eeeb69662e1e0ebad99c0d4c7cc7287d0
Gaston Dombiak <gdombiak@gmail.com>
2018-07-15 12:58:25 -0700

    Cleaned up code

--- OctoPod/Setup UI/PrintersTableViewController.swift
+++ OctoPod/Setup UI/PrintersTableViewController.swift
@@ -9,12 +9,6 @@ class PrintersTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-        
         printers = printerManager.getPrinters()
     }
     
@@ -40,12 +34,6 @@ class PrintersTableViewController: UITableViewController {
         cell.detailTextLabel?.text = printers[indexPath.row].hostname
 
         return cell
-    }
-
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
     }
 
     // Override to support editing the table view.
