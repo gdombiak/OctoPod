82b2aea1cb216e8a2b9fc4a774f14742d0bd4077
Gaston Dombiak <gdombiak@gmail.com>
2018-09-08 22:51:25 -0700

    Implemented and tested "app locked" mode. Added biometrics/passcode for security to unlock app
    Closes #60

--- OctoPod/AppConfiguration.swift
+++ OctoPod/AppConfiguration.swift
@@ -3,9 +3,29 @@ import Foundation
 class AppConfiguration {
     
     private static let APP_LOCKED = "APP_CONFIGURATION_LOCKED"
+    private static let APP_LOCKED_AUTHENTICATION = "APP_CONFIGURATION_LOCKED_AUTHENTICATION"
 
     // MARK: - App locking
     
+    // Returns true if user needs to authenticate (using biometrics or passcode)
+    // to unlock the app (i.e. allow "write" operations)
+    func appLockedRequiresAuthentication() -> Bool {
+        let defaults = UserDefaults.standard
+        if let requires = defaults.object(forKey: AppConfiguration.APP_LOCKED_AUTHENTICATION) as? Bool {
+            return requires
+        }
+        // A value does not exist so default to true
+        appLockedRequiresAuthentication(requires: true)
+        return true
+    }
+    
+    // Sets whether the user needs to authenticate (using biometrics or passcode)
+    // to unlock the app (i.e. allow "write" operations) or not
+    func appLockedRequiresAuthentication(requires: Bool) {
+        let defaults = UserDefaults.standard
+        return defaults.set(requires, forKey: AppConfiguration.APP_LOCKED_AUTHENTICATION)
+    }
+
     // Returns true if the app is in a locked state. This means that
     // read-only operations are only allowed. Operations like changing
     // print settings, printer settings, change running jobs, .etc
--- OctoPod/Settings UI/SecurityViewController.swift
+++ OctoPod/Settings UI/SecurityViewController.swift
@@ -1,10 +1,13 @@
 import UIKit
+import LocalAuthentication
 
 class SecurityViewController: ThemedStaticUITableViewController {
 
     let appConfiguration: AppConfiguration = { return (UIApplication.shared.delegate as! AppDelegate).appConfiguration }()
 
+    @IBOutlet weak var appLockBiometricsLabel: UILabel!
     @IBOutlet weak var appLockLabel: UILabel!
+    @IBOutlet weak var appLockBiometricsSwitch: UISwitch!
     @IBOutlet weak var appLockSwitch: UISwitch!
     
     override func viewDidLoad() {
@@ -14,10 +17,15 @@ class SecurityViewController: ThemedStaticUITableViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        // Theme labels
         let theme = Theme.currentTheme()
+        appLockBiometricsLabel.textColor = theme.textColor()
         appLockLabel.textColor = theme.textColor()
 
+        // Configure state of switches
+        appLockBiometricsSwitch.isOn = appConfiguration.appLockedRequiresAuthentication()
         appLockSwitch.isOn = appConfiguration.appLocked()
+        reviewBiometricsSwitch()
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,7 +34,53 @@ class SecurityViewController: ThemedStaticUITableViewController {
     }
 
 
+    // MARK: - App Lock switches
+    
+    @IBAction func appLockBiometricsChanged(_ sender: Any) {
+        appConfiguration.appLockedRequiresAuthentication(requires: appLockBiometricsSwitch.isOn)
+    }
+    
     @IBAction func appLockChanged(_ sender: Any) {
-        appConfiguration.appLocked(locked: appLockSwitch.isOn)
+        if !appLockSwitch.isOn && appLockBiometricsSwitch.isOn {
+            // User needs to authenticate to be able to disable read-only mode
+            authenticateToUnlock()
+        } else {
+            appConfiguration.appLocked(locked: appLockSwitch.isOn)
+            reviewBiometricsSwitch()
+        }
+    }
+    
+    // MARK: - Private functions
+    
+    fileprivate func authenticateToUnlock() {
+        let localAuthenticationContext = LAContext()
+        localAuthenticationContext.localizedFallbackTitle = "Use Passcode"
+        
+        var authError: NSError?
+        let reasonString = "To disable read-only mode"
+        
+        if localAuthenticationContext.canEvaluatePolicy(.deviceOwnerAuthentication, error: &authError) {
+            
+            localAuthenticationContext.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reasonString) { success, evaluateError in
+                
+                if success {
+                    // User authenticated successfully, take appropriate action
+                    DispatchQueue.main.async {
+                        self.appConfiguration.appLocked(locked: false)
+                        self.reviewBiometricsSwitch()
+                    }
+                } else {
+                    // User failed to authenticate so revert change
+                    DispatchQueue.main.async {
+                        self.appLockSwitch.isOn = true
+                    }
+                }
+            }
+        }
+    }
+
+    fileprivate func reviewBiometricsSwitch() {
+        // Do not let user change this setting when app is in locked mode
+        appLockBiometricsSwitch.isEnabled = !appLockSwitch.isOn
     }
 }
