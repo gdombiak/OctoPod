fb6d817ceab646cbc4ea5c530c8c1e5eeb3588e5
Gaston Dombiak <gdombiak@gmail.com>
2018-10-05 00:50:48 -0700

    Added option to choose iOS language or keep the app in english. Italian and Czech translations are pending
    Closes #96

--- OctoPod/Settings UI/AppearanceViewController.swift
+++ OctoPod/Settings UI/AppearanceViewController.swift
@@ -1,9 +1,12 @@
 import UIKit
 
 class AppearanceViewController: ThemedStaticUITableViewController {
+    
+    private static let APP_SYSTEM_LANGUAGE_OVERRIDE = "APP_SYSTEM_LANGUAGE_OVERRIDE"
 
     @IBOutlet weak var lightCell: UITableViewCell!
     @IBOutlet weak var darkCell: UITableViewCell!
+    @IBOutlet weak var languageControl: UISegmentedControl!
     
     @IBOutlet weak var lightLabel: UILabel!
     @IBOutlet weak var darkLabel: UILabel!
@@ -23,9 +26,17 @@ class AppearanceViewController: ThemedStaticUITableViewController {
         lightLabel.textColor = theme.textColor()
         darkLabel.textColor = theme.textColor()
         refreshSelectedTheme(theme: theme)
+        
+        // Set language being used
+        let languageOverride = UserDefaults.standard.object(forKey: AppearanceViewController.APP_SYSTEM_LANGUAGE_OVERRIDE) != nil
+        languageControl.selectedSegmentIndex = languageOverride ? 1 : 0
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section != 0 {
+            // Ignore if not selecting Theme
+            return
+        }
         if indexPath.row == 0 {
             Theme.switchTheme(choice: Theme.ThemeChoice.Light)
         } else {
@@ -47,5 +58,17 @@ class AppearanceViewController: ThemedStaticUITableViewController {
         
         lightCell.accessoryType = lightSelected ? .checkmark : .none
         darkCell.accessoryType = lightSelected ? .none : .checkmark
+    }
+    
+    @IBAction func languageChanged(_ sender: Any) {
+        let languageKey = "AppleLanguages"
+        let defaults = UserDefaults.standard
+        if languageControl.selectedSegmentIndex == 0 {
+            defaults.removeObject(forKey: languageKey)
+            defaults.removeObject(forKey: AppearanceViewController.APP_SYSTEM_LANGUAGE_OVERRIDE)
+        } else {
+            defaults.set(["en"], forKey: languageKey)
+            defaults.set(true, forKey: AppearanceViewController.APP_SYSTEM_LANGUAGE_OVERRIDE)
+        }
     }
 }
