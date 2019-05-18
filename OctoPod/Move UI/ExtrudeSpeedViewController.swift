f98101f242f899c8582c62dd5e48632265f4085f
Gaston Dombiak <gdombiak@gmail.com>
2019-05-18 13:54:45 -0700

    Improved UX for custom speed. Closes #183

--- OctoPod/Move UI/ExtrudeSpeedViewController.swift
+++ OctoPod/Move UI/ExtrudeSpeedViewController.swift
@@ -13,32 +13,30 @@ class ExtrudeSpeedViewController: ThemedStaticUITableViewController {
     }
 
     @IBAction func speedChoiceChanged(_ sender: Any) {
-        let isCustom = isCustomSpeed()
-        customSpeedField.isEnabled = isCustom
-        if isCustom  {
+        if isCustomSpeed()  {
             if let value = customSpeedField.text {
                 proceedButton.isEnabled = !value.isEmpty
             } else {
                 proceedButton.isEnabled = false
             }
+            // "Move cursor" to custom speed field
+            customSpeedField.becomeFirstResponder()
         } else {
             proceedButton.isEnabled = true
+            // Remove cursor from custom field
+            customSpeedField.resignFirstResponder()
         }
     }
     
+    @IBAction func customSpeedTouched(_ sender: Any) {
+        // Make sure that Custom is selected when user clicked on field to enter Speed value
+        speedChoiceControl.selectedSegmentIndex = 1
+        // Enable/Disable Proceed button based on value
+        enableProceedBasedOnCustomSpeed()
+    }
+    
     @IBAction func customSpeedChanging(_ sender: Any) {
-        if let value = customSpeedField.text {
-            proceedButton.isEnabled = !value.isEmpty
-            if let speed = Int(value) {
-                if speed < 0 {
-                    customSpeedField.text = "0"
-                } else if speed > 3000 {
-                    customSpeedField.text = "3000"
-                }
-            }
-        } else {
-            proceedButton.isEnabled = false
-        }
+        enableProceedBasedOnCustomSpeed()
     }
     
     @IBAction func proceedClicked(_ sender: Any) {
@@ -56,5 +54,18 @@ class ExtrudeSpeedViewController: ThemedStaticUITableViewController {
         return speedChoiceControl.selectedSegmentIndex == 1
     }
     
-
+    fileprivate func enableProceedBasedOnCustomSpeed() {
+        if let value = customSpeedField.text {
+            proceedButton.isEnabled = !value.isEmpty
+            if let speed = Int(value) {
+                if speed < 0 {
+                    customSpeedField.text = "0"
+                } else if speed > 3000 {
+                    customSpeedField.text = "3000"
+                }
+            }
+        } else {
+            proceedButton.isEnabled = false
+        }
+    }
 }
