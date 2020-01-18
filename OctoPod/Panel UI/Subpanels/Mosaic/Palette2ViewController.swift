355ffd71c6431f1e8d8ec66aca3e1c0792d86eb7
Gaston Dombiak <gdombiak@gmail.com>
2020-01-18 13:11:38 -0800

    Added Ping/Pong variance stats
    Closes #245

--- OctoPod/Panel UI/Subpanels/Mosaic/Palette2ViewController.swift
+++ OctoPod/Panel UI/Subpanels/Mosaic/Palette2ViewController.swift
@@ -277,7 +277,7 @@ class Palette2ViewController: ThemedStaticUITableViewController, SubpanelViewCon
                         }
                     } else {
                         // Show latest ping values
-                        if let messages = Palette2ViewController.pingPongMessage(entry: pings.last!) {
+                        if let messages = Palette2ViewController.pingPongMessage(history: pings, index: pings.count - 1, reversed: false) {
                             DispatchQueue.main.async {
                                 self.latestPingNumberValueLabel.text = messages.number
                                 self.latestPingOffsetValueLabel.text = messages.percent
@@ -296,7 +296,7 @@ class Palette2ViewController: ThemedStaticUITableViewController, SubpanelViewCon
                         }
                     } else {
                         // Show latest pong values
-                        if let messages = Palette2ViewController.pingPongMessage(entry: pongs.last!) {
+                        if let messages = Palette2ViewController.pingPongMessage(history: pongs, index: pongs.count - 1, reversed: false) {
                             DispatchQueue.main.async {
                                 self.latestPongNumberValueLabel.text = messages.number
                                 self.latestPongOffsetValueLabel.text = messages.percent
@@ -418,15 +418,58 @@ class Palette2ViewController: ThemedStaticUITableViewController, SubpanelViewCon
     
     // MARK: - Static functions
     
-    static func pingPongMessage(entry: Dictionary<String,Any>) -> (number: String, percent: String)? {
-        if let number = entry["number"] as? Int, let percent = entry["percent"] as? String {
-            return ("\(number)" , percent == "MISSED" ? NSLocalizedString("Missed", comment: "") : "\(percent) %")
-        } else if let number = entry["number"] as? Int, let percent = entry["percent"] as? Double {
-            // New version 2.3.1 of Palette 2 plugin uses Float(in Python) for sending percent. It used to be a String in previous versions.
-            // String is still used for indicating MISSED in this version
-            return ("\(number)" , "\(String(format: "%.2f", percent)) %")
+    static func pingPongMessage(history: Array<Dictionary<String,Any>>, index: Int, reversed: Bool) -> (number: String, percent: String, variance: String)? {
+        if history.count > index {
+            let entry = history[index]
+            var variance = "-- %"
+            if let number = entry["number"] as? Int, let percent = entry["percent"] as? String {
+                return ("\(number)" , percent == "MISSED" ? NSLocalizedString("Missed", comment: "") : "\(percent) %", variance)
+            } else if let number = entry["number"] as? Int, let percent = entry["percent"] as? Double {
+                // New version 2.3.1 of Palette 2 plugin uses Float(in Python) for sending percent. It used to be a String in previous versions.
+                // String is still used for indicating MISSED in this version
+                if reversed {
+                    if index < history.count - 1, let previousPrct = history[index + 1]["percent"] as? Double {
+                        variance = "\(String(format: "%.2f", abs(percent - previousPrct)))%"
+                    }
+                } else {
+                    if index > 0, let previousPrct = history[index - 1]["percent"] as? Double {
+                        variance = "\(String(format: "%.2f", abs(percent - previousPrct)))%"
+                    }
+                }
+                return ("\(number)" , "\(String(format: "%.2f", percent))%", variance)
+            }
         }
         return nil
+    }
+
+    static func pingPongVarianceStats(history: Array<Dictionary<String,Any>>, reversed: Bool) -> (max: String, average: String, min: String)? {
+        var maxVariance = 0.0, totalVariance = 0.0, countVariance = 0.0, minVariance = 0.0
+        for (index, entry) in history.enumerated() {
+            var variance: Double?
+            if let percent = entry["percent"] as? Double {
+                if reversed {
+                    if index < history.count - 1, let previousPrct = history[index + 1]["percent"] as? Double {
+                        variance = abs(percent - previousPrct)
+                    }
+                } else {
+                    if index > 0, let previousPrct = history[index - 1]["percent"] as? Double {
+                        variance = abs(percent - previousPrct)
+                    }
+                }
+                if let variance = variance {
+                    countVariance += 1
+                    totalVariance += variance
+                    if maxVariance < variance {
+                        maxVariance = variance
+                    }
+                    if minVariance == 0 || minVariance > variance {
+                        minVariance = variance
+                    }
+                }
+            }
+        }
+        let averageVariance = countVariance > 0 ? totalVariance / countVariance : 0.0
+        return ("\(String(format: "%.2f", maxVariance))%", "\(String(format: "%.2f", averageVariance))%", "\(String(format: "%.2f", minVariance))%")
     }
 }
 
--- OctoPod/Panel UI/Subpanels/Mosaic/PingPongHistoryViewController.swift
+++ OctoPod/Panel UI/Subpanels/Mosaic/PingPongHistoryViewController.swift
@@ -1,32 +1,99 @@
 import UIKit
 
-class PingPongHistoryViewController: ThemedDynamicUITableViewController {
+class PingPongHistoryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var varianceStatsView: UIView!
+    
+    @IBOutlet weak var maxTextLabel: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
+    @IBOutlet weak var averageTextLabel: UILabel!
+    @IBOutlet weak var averageLabel: UILabel!
+    @IBOutlet weak var minTextLabel: UILabel!
+    @IBOutlet weak var minLabel: UILabel!
+    
     var history: Array<Dictionary<String,Any>>?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        ThemeUIUtils.applyTheme(table: tableView, staticCells: false)
+        applyTheme()
+        // To force repaint of theme when coming back straight from a new Appearance
+        self.tableView.reloadData()
+        if let history = history, let stats = Palette2ViewController.pingPongVarianceStats(history: history, reversed: true) {
+            maxLabel.text = stats.max
+            averageLabel.text = stats.average
+            minLabel.text = stats.min
+        } else {
+            maxLabel.text = "0.0%"
+            averageLabel.text = "0.0%"
+            minLabel.text = "0.0%"
+        }
+    }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return history?.count ?? 0
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let theme = Theme.currentTheme()
+        let textColor = theme.textColor()
+
         let cell = tableView.dequeueReusableCell(withIdentifier: "history_cell", for: indexPath)
 
         // Configure the cell...
-        if let entry = history?[indexPath.row], let messages = Palette2ViewController.pingPongMessage(entry: entry) {
-            cell.textLabel?.text = "# " + messages.number
-            cell.detailTextLabel?.text = messages.percent
+        if let history = history, let messages = Palette2ViewController.pingPongMessage(history: history, index: indexPath.row, reversed: true) {
+            
+            if let numberLabel = cell.viewWithTag(100) as? UILabel {
+                numberLabel.text = "# " + messages.number
+                numberLabel.textColor = textColor
+            }
+
+            if let prctLabel = cell.viewWithTag(101) as? UILabel {
+                prctLabel.text = messages.percent
+                prctLabel.textColor = textColor
+            }
+
+            if let varianceLabel = cell.viewWithTag(102) as? UILabel {
+                varianceLabel.text = messages.variance
+                varianceLabel.textColor = textColor
+            }
         }
 
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        ThemeUIUtils.themeCell(cell: cell)
+    }
+    
+    // MARK: - Theme functions
+
+    fileprivate func applyTheme() {
+        let theme = Theme.currentTheme()
+        let textLabelColor = theme.labelColor()
+        let textColor = theme.textColor()
+        
+        varianceStatsView.backgroundColor = theme.backgroundColor()
+        
+        maxTextLabel.textColor = textLabelColor
+        averageTextLabel.textColor = textLabelColor
+        minTextLabel.textColor = textLabelColor
+
+        maxLabel.textColor = textColor
+        averageLabel.textColor = textColor
+        minLabel.textColor = textColor
+
     }
 }
